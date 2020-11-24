75fe0164eb8aeb69819d6f9d5dcd59e569fcc89a
ShroXd <ShroXdF@gmail.com>
2020-11-24 20:18:08 +0800

    fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,35 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import typescript from "rollup-plugin-typescript2";
-import sourceMaps from "rollup-plugin-sourcemaps";
-import { terser } from 'rollup-plugin-terser'
+import path from 'path'
+import typescript from 'rollup-plugin-typescript2'
+import isBuiltinModule from 'is-builtin-module'
+
+function resolveInput(projectDir) {
+  return path.resolve('packages', `${projectDir}/lib/index.ts`)
+}
+
+function resolveOnput(projectDir) {
+  return path.resolve('packages', `${projectDir}/dist/index.js`)
+}
+
+const PKG_DIR = process.env.PKG_DIR
+const pkgMeta = require(path.resolve(`packages`, `${PKG_DIR}/package.json`))
 
 export default {
-    input: "./src/index.ts",
-    output: [
-        {
-            format: "umd",
-            file: "lib/bundle.umd.js",
-            name: "throttle-debounce-ts",
-            sourcemap: true
-        },
-        {
-            format: "es",
-            file: "lib/bundle.esm.js",
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        resolve(),
-        commonjs(),
-        typescript({
-            useTsconfigDeclarationDir: true
-        }),
-        sourceMaps(),
-        terser()
-    ],
-    external: []
-};
+  input: resolveInput(PKG_DIR),
+  external(id) {
+    return pkgMeta.dependencies && !!pkgMeta.dependencies[id]
+  },
+  plugins: [
+    typescript({
+      cacheRoot: path.resolve(__dirname, 'node_modules/.rts2_cache')
+    })
+  ],
+  output: {
+    file: resolveOnput(PKG_DIR),
+    format: 'cjs'
+  },
+  onwarn(warning, warn) {
+    if (warning.code === 'UNRESOLVED_IMPORT' && isBuiltinModule(warning.source))
+      return
+    warn(warning)
+  }
+}
