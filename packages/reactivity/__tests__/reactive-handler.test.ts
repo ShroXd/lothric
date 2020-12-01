0e3d2d669d0818c0799635c26b0383a2c2855319
ShroXd <ShroXdF@gmail.com>
2020-12-01 10:01:01 +0800

    refactor(reactive-handler.test.ts): use beforeEach function to reduce duplication test codes

--- packages/reactivity/__tests__/reactive-handler.test.ts
+++ packages/reactivity/__tests__/reactive-handler.test.ts
@@ -1,31 +1,33 @@
 import { Reactivity } from '../lib';
 
 describe('@lothric/reactivity/reactive-handler.ts (base test case)', () => {
+  let defaultMembrane: Reactivity;
+  beforeEach(() => {
+    defaultMembrane = new Reactivity();
+  });
+
   it('should idempotent', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet1 = membrane.reactive(raw);
-    const wet2 = membrane.reactive(raw);
+    const wet1 = defaultMembrane.reactive(raw);
+    const wet2 = defaultMembrane.reactive(raw);
     expect(wet1).toStrictEqual(wet2);
     expect(wet1.a).toStrictEqual(wet2.a);
   });
 
   it('should cache proxy', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet1 = membrane.reactive(raw);
-    const wet2 = membrane.reactive(wet1);
+    const wet1 = defaultMembrane.reactive(raw);
+    const wet2 = defaultMembrane.reactive(wet1);
     expect(wet1).toStrictEqual(wet2);
     expect(wet1.a).toStrictEqual(wet2.a);
   });
 
   it('should handle frozen objects correctly', () => {
     const raw = Object.freeze({ a: 1 });
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     expect(() => {
       wet.a;
     }).not.toThrowError();
@@ -35,8 +37,7 @@ describe('@lothric/reactivity/reactive-handler.ts (base test case)', () => {
   });
 
   it('should handle deep nested mutations correctly', () => {
-    const membrane = new Reactivity();
-    const wet = membrane.reactive({});
+    const wet = defaultMembrane.reactive({});
     expect(() => {
       wet.a = 1;
     }).not.toThrowError();
@@ -72,19 +73,22 @@ describe('@lothric/reactivity/reactive-handler.ts (base test case)', () => {
 });
 
 describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
+  let defaultMembrane: Reactivity;
+  beforeEach(() => {
+    defaultMembrane = new Reactivity();
+  });
+
   it('should handle has correctly', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     expect('a' in wet);
   });
 
   it('should handle delete correctly', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     expect('a' in wet).toBeTruthy();
     delete wet.a;
     expect('a' in wet).not.toBeTruthy();
@@ -98,26 +102,23 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
 
   it('should handle Object.defineProperty correctly', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     Object.defineProperty(wet, 'b', { value: 2 });
     expect(wet.b).toBe(2);
   });
 
   it('should assign same property with value on original object', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     Object.defineProperty(wet, 'b', { value: 2 });
   });
 
   it('should handle Object.defineProperty correctly with undefined non-configurable descriptor', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     Object.defineProperty(wet, 'b', {
       value: undefined,
       configurable: false,
@@ -130,9 +131,8 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
 
   it('should handle Object.defineProperty correctly with non-configurable descriptor', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     Object.defineProperty(wet, 'b', {
       value: 2,
       configurable: false,
@@ -145,9 +145,8 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
 
   it('should not allow deleting non-configurable property', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     Object.defineProperty(wet, 'b', {
       value: 2,
       configurable: false,
@@ -160,9 +159,8 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
 
   it('should not allow re-defining non-configurable property', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     Object.defineProperty(wet, 'b', {
       value: 2,
       configurable: false,
@@ -177,17 +175,15 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
 
   it('shoud handle Object.isExtensible correctly', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     expect(Object.isExtensible(wet)).toBeTruthy();
   });
 
   it('should handle Object.preventExtensions correctly', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     expect(() => {
       Object.preventExtensions(wet);
     }).not.toThrow();
@@ -200,9 +196,8 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
   it('should throw error when user invoke Object.setPrototypeOf', () => {
     const raw = { a: 1 };
     const newProto = { b: 2 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     expect(() => {
       Object.setPrototypeOf(wet, newProto);
     }).toThrowError();
@@ -210,9 +205,8 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
 
   it('should handle Object.getOwnPropertyNames correctly', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     const names = Object.getOwnPropertyNames(wet);
     expect(names.length).toBe(1);
     expect(names[0]).toBe('a');
@@ -223,9 +217,8 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
     const raw = {
       [sym]: 'value of symbol key',
     };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     const names = Object.getOwnPropertyNames(wet);
     expect(names.length).toBe(0);
   });
@@ -236,9 +229,8 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
       a: 1,
       [sym]: 'value of symbol key',
     };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     const names = Object.getOwnPropertyNames(wet);
     expect(names.length).toBe(1);
     expect(names[0]).toBe('a');
@@ -247,17 +239,15 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
   it('should handle Object.getOwnPropertySymbols correctly', () => {
     const sym = Symbol();
     const raw = { [sym]: 'value of symbol key' };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     expect(Object.getOwnPropertySymbols(wet)).toEqual([sym]);
   });
 
   it('should handle Object.getOwnPropertySymbols correctly when object has key', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     const symbols = Object.getOwnPropertySymbols(wet);
     expect(symbols.length).toEqual(0);
   });
@@ -268,9 +258,8 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
       a: 1,
       [sym]: 'value of symbol key',
     };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     const symbols = Object.getOwnPropertySymbols(wet);
     expect(symbols.length).toEqual(1);
     expect(symbols[0]).toEqual(sym);
@@ -282,23 +271,21 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
       a: 1,
       [sym]: 'value of symbol key',
     };
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     expect(Object.keys(wet)).toStrictEqual(['a']);
   });
 
   it('should return reactive proxy when access property value via data descriptor', () => {
     const raw1 = { a: 1 };
     const raw2 = { b: 2 };
-    const membrane = new Reactivity();
 
     Object.defineProperty(raw1, 'raw2', {
       value: raw2,
       configurable: true,
     });
-    const reactiveRaw1 = membrane.reactive(raw1);
-    const reactiveRaw2 = membrane.reactive(raw2);
+    const reactiveRaw1 = defaultMembrane.reactive(raw1);
+    const reactiveRaw2 = defaultMembrane.reactive(raw2);
     expect(reactiveRaw1.raw2).toStrictEqual(reactiveRaw2);
 
     const descriptor = Object.getOwnPropertyDescriptor(reactiveRaw1, 'raw2');
@@ -308,7 +295,6 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
   it('should return reactive proxy when access property value via accessor descriptor', () => {
     const raw1 = { a: 1 };
     const raw2 = { b: 2 };
-    const membrane = new Reactivity();
 
     Object.defineProperty(raw1, 'raw2', {
       get() {
@@ -316,8 +302,8 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
       },
       configurable: true,
     });
-    const reactiveRaw1 = membrane.reactive(raw1);
-    const reactiveRaw2 = membrane.reactive(raw2);
+    const reactiveRaw1 = defaultMembrane.reactive(raw1);
+    const reactiveRaw2 = defaultMembrane.reactive(raw2);
     expect(reactiveRaw1.raw2).toStrictEqual(reactiveRaw2);
 
     const descriptor = Object.getOwnPropertyDescriptor(reactiveRaw1, 'raw2');
@@ -326,7 +312,6 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
 
   it('should allow user set invocation via descriptor', () => {
     const raw = { a: 1 };
-    const membrane = new Reactivity();
     let num = 2;
     const newNum = 3;
 
@@ -339,7 +324,7 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
       },
       configurable: true,
     });
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     const descriptor = Object.getOwnPropertyDescriptor(wet, 'num');
     const set = descriptor!.set!;
     const get = descriptor!.get!;
@@ -351,9 +336,8 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
   it('should allow user handle property value via descriptor which only have getter/setter', () => {
     const raw = { a: 1 };
     let num = 2;
-    const membrane = new Reactivity();
 
-    const wet = membrane.reactive(raw);
+    const wet = defaultMembrane.reactive(raw);
     Object.defineProperty(wet, 'b', {
       get() {
         return num;
@@ -373,11 +357,16 @@ describe('@lothric/reactivity/reactive-handler.ts (handler)', () => {
 });
 
 describe('@lothric/reactivity/reactive-handler.ts (Array)', () => {
-  it('should handle Array correctly', () => {
-    const raw: any[] = [];
-    const membrane = new Reactivity();
+  let defaultMembrane: Reactivity;
+  beforeEach(() => {
+    defaultMembrane = new Reactivity();
+  });
 
-    const wet = membrane.reactive(raw);
+  it('should wrap Array correctly', () => {
+    const raw: any[] = [];
+    const wet = defaultMembrane.reactive(raw);
     expect(wet).not.toBe(raw);
   });
+
+  it('shoud allow access item in array', () => {});
 });
